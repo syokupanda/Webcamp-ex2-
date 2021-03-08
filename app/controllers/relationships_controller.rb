07208b9bbb35bd28a57ee22cd0a37993274ae360
syokupanda <borun.books@gmail.com>
2021-03-08 03:45:59 +0000

    [FinishPlus]ex4

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
 
   def follow
     current_user.follow(params[:id])
-    redirect_to users_path
+    redirect_back(fallback_location:users_path)
   end
 
   def unfollow
     current_user.unfollow(params[:id])
-    redirect_to users_path
+    redirect_back(fallback_location:users_path)
   end
 
   def follower_index
